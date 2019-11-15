de984fcccb0b3906e74510d0e647e6a67e3b779c
Blues <holyblues@gmail.com>
2019-11-16 00:57:26 +0800

    add connect to socket sample code

--- src/main.go
+++ src/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
 	"path"
 	"text/template"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -21,6 +26,8 @@ var upgrader = websocket.Upgrader{
 		return true
 	},
 }
+
+var wsAddr = flag.String("addr", "localhost:8080", "http service address")
 
 // Message Define our message object
 type Message struct {
@@ -134,4 +141,66 @@ func chatRoom(w http.ResponseWriter, r *http.Request) {
 func sample(w http.ResponseWriter, r *http.Request) {
 	fp := path.Join("../public", "sample.html")
 	render(w, r, fp)
+}
+
+func dial() {
+	flag.Parse()
+	log.SetFlags(0)
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	u := url.URL{Scheme: "ws", Host: *wsAddr, Path: "/echo"}
+	log.Printf("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+			log.Printf("recv: %s", message)
+		}
+	}()
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-done:
+			return
+		case t := <-ticker.C:
+			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
+			if err != nil {
+				log.Println("write:", err)
+				return
+			}
+		case <-interrupt:
+			log.Println("interrupt")
+
+			// Cleanly close the connection by sending a close message and then
+			// waiting (with timeout) for the server to close the connection.
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("write close:", err)
+				return
+			}
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
+			return
+		}
+	}
 }
